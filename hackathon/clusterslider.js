23c2096da7cc25f705a80f44f48db6bf7b27ad37
Alexandra Svatikova <alexandra.svatikova@corp.mail.ru>
2017-11-13 19:16:55 +0300

    hackathon

--- hackathon/clusterslider.js
+++ hackathon/clusterslider.js
@@ -1,24 +1,40 @@
 function ClusterSlider(id) {
     this.input = document.getElementById(id);
-    this.labelNetwork = document.getElementById(id + '-network');
-    this.labelHardware = document.getElementById(id + '-hardware');
+    this.labelNetwork = document.getElementById(id + '-label-network');
+    this.labelHardware = document.getElementById(id + '-label-hardware');
 
     this.valueToCluster = {
         '0' : {
             network: 2,
-            hardware: 3
+            hardware: 2
         },
         '1': {
             network: 2,
             hardware: 3
         },
         '2': {
-            network: 2,
-            hardware: 3
+            network: 3,
+            hardware: 2
         },
         '3': {
-            network: 2,
+            network: 3,
             hardware: 3
+        },
+        '4' : {
+            network: 6,
+            hardware: 6
+        },
+        '5': {
+            network: 6,
+            hardware: 7
+        },
+        '6': {
+            network: 7,
+            hardware: 6
+        },
+        '7': {
+            network: 7,
+            hardware: 7
         }
     };
 
