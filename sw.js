27393da3b4b2c63ec5e56aa12ab6435aab726328
Alexandra Svatikova <alexandra.svatikova@corp.mail.ru>
2019-09-04 12:43:45 +0300

    111

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+    console.log("Service worker install");
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+    console.log("Service worker activate");
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.url.includes("getSavedUsers")) {
+        console.log("Service worker request " + event.request.url);
+        
+    event.respondWith(
+        return fetch(event.request).then(response => {
+                console.log("Service worker response\n" + event.request.url);  
+                return response;
+            });
+          );
+      }
+});
